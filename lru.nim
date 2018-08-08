f3a9c06a16ebec5a736606ceb8232b6294283afe
gogolxdong <lxdong.007@gmail.com>
2018-08-08 14:13:46 +0800

    update

--- lru.nim
+++ lru.nim
@@ -4,10 +4,10 @@ type EvictCallback = proc(key: string, value: string)
 
 type LRU* = object
     size  :    int
-    evictList: DoublyLinkedList
-    items  :   Table[string,  DoublyLinkedNode]
+    evictList: DoublyLinkedList[pointer]
+    items  :   Table[string,  DoublyLinkedNode[pointer]]
     onEvict :  EvictCallback
 
 
-type Cache = object
+type Cache* = object
     lru  :LRU
