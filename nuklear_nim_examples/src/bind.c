341b6340ef3442793a038a281e13c6e4271781cf
gogolxdong <lxdong.007@gmail.com>
2018-09-20 14:25:52 +0800

    update

---
+++ nuklear_nim_examples/src/bind.c
@@ -0,0 +1,14 @@
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_IMPLEMENTATION
+
+#include "nuklear/nuklear.h"
+
+extern int nk_filter_my(const struct nk_text_edit *box, nk_rune unicode) {
+	return 0;
+}
