2fb59db3d52b3ccc9eeb4e6b919e09fdfb512d5f
gogolxdong <lxdong.007@gmail.com>
2018-11-13 19:20:14 +0800

    update

---
+++ nimquic/server.nim
@@ -0,0 +1,24 @@
+import nativesockets, os, binaryparse, streams
+
+let sockfd = createNativeSocket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+var peer = getAddrInfo("192.168.100.224", Port 1234, sockType = SOCK_DGRAM, protocol =  IPPROTO_UDP)
+echo sockfd.bindAddr(peer.ai_addr, peer.ai_addrlen)
+
+createParser(versionNegoPacket):
+    u8: headerType 
+    u32: version 
+    u4: dcil 
+    u4: scil
+    u8: dcid[if dcil != 0: dcil+3 else: 0]
+    u8: scid[if scil != 0: scil+3 else: 0]
+    u32: supportedVersion[]
+
+var buf  : array[15, byte]
+while true:
+    var numbytes = recvfrom(sockfd, addr buf, sizeof buf, 0, nil, nil)
+    echo buf   
+    var content = $cast[cstring](addr buf[0])
+    echo content
+    var ss = newStringStream( content)
+    var readData = versionNegoPacket.get(ss)
+    echo readData
