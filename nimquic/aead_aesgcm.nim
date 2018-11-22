cfbb6d2f5bc5cd9539679bdeb10a13f5126521c8
root <root@OFFICE.localdomain>
2018-11-22 19:13:37 +0800

    update

--- nimquic/aead_aesgcm.nim
+++ nimquic/aead_aesgcm.nim
@@ -2,7 +2,7 @@ import nimssl
 import hkdf
 
 proc computeSecrets(connID: string): tuple[clientSecret, serverSecret:seq[byte]] =
-    var initialSecret = hkdfExtract(SHA256, connID, quicVersion1Salt)
-    clientSecret = HkdfExpandLabel(crypto.SHA256, initialSecret, "client in", 32)
-    serverSecret = HkdfExpandLabel(crypto.SHA256, initialSecret, "server in", 32)
+    var initialSecret = hkdfExtract(connID, cast[string](quicVersion1Salt))
+    result.clientSecret = hkdfExpandLabel(initialSecret, "client in", 32)
+    result.serverSecret = hkdfExpandLabel(initialSecret, "server in", 32)
     
--- nimquic/hkdf.nim
+++ nimquic/hkdf.nim
@@ -1,12 +1,12 @@
 import sequtils, strutils, endians
 import nimcrypto 
 
-var quicVersion1Salt = [0x9c'u8, 0x10, 0x8f, 0x98, 0x52, 
-                        0x0a, 0x5c, 0x5c, 0x32, 0x96,
+var quicVersion1Salt* = @[0x9c'u8, 0x10, 0x8f, 0x98, 0x52, 
+                         0x0a, 0x5c, 0x5c, 0x32, 0x96,
                          0x8e, 0x95, 0x0e, 0x8a, 0x2c, 
                          0x5f, 0xe0, 0x6d, 0x6c, 0x38]
 
-proc hkdfExpand*(prk, info: seq[uint8], l: int): seq[byte] =
+proc hkdfExpand*(prk, info: string, l: int): seq[byte] =
     var 
         expander = sha256.hmac(prk, info)
         res: seq[byte]
@@ -26,24 +26,19 @@ proc hkdfExpand*(prk, info: seq[uint8], l: int): seq[byte] =
     #     p = p[n:]
     result = res
 
-proc hkdfExpandLabel*(secret: seq[byte], label: string, length: int): seq[byte] =
+proc hkdfExpandLabel*(secret: string, label: string, length: int): seq[byte] =
     const prefix = "quic "
-    var qlabel: seq[uint8] 
+    var qlabel: string
     var n = length
     bigEndian16(addr qlabel[0], addr n)
     bigEndian16(addr qlabel[1], addr n)
-    qlabel[2] = uint8 prefix.len + label.len
-    qlabel.add cast[seq[uint8]](prefix & label)
+    qlabel[2] = char prefix.len + label.len
+    qlabel.add prefix & label
     result = hkdfExpand(secret, qlabel, length)
 
-proc hkdfExtract*(key: ptr byte, keylen:uint, data: ptr byte, datalen: uint): string =
-    result = $sha256.hmac(key, keylen, data, datalen)
+proc hkdfExtract*(key: string, data: string): string =
+    result = $sha256.hmac(key, data)
 
 
 when isMainModule:
-    var stringToHmac = "a"
-    let data = cast[ptr byte](addr stringToHmac[0])
-    let datalen = uint(len(stringToHmac))
-    let key = cast[ptr byte](addr quicVersion1Salt[0])
-    let keylen = uint(len(quicVersion1Salt))
-    echo hkdfExtract(key, keylen, data, datalen)
+    echo hkdfExtract(cast[string](quicVersion1Salt), "a")
